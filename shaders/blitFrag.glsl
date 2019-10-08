f021dffb2318e1bc83277669dc34cc3569dacaf3
Pauli <pauli.kemppinen@aalto.fi>
2019-10-08 19:32:19 +0300

    fixed obj rendering element count :DD and added support for non-triangles

--- shaders/blitFrag.glsl
+++ shaders/blitFrag.glsl
@@ -9,9 +9,9 @@ out vec3 color;
 void main() {
 	vec2 coord = uv * .5 + vec2(.5);
 	if (coord.x < 1. / 3.)
-		color = vec3(exp(-2. / abs(texture(depth, coord).x)));
+		color = vec3(abs(-.005 / abs(texture(depth, coord).x-1.)));
 	else if (coord.x < 2. / 3.)
-		color = texture(normal, coord).xyz;
+		color = texture(normal, coord).xyz*.5+vec3(.5);
 	else
 		color = texture(albedo, coord).xyz;
 }
