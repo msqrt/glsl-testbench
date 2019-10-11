61ca922279c6912ab1c5f1031c90e2e50b87643a
Pauli <pauli.kemppinen@aalto.fi>
2019-10-12 01:21:03 +0300

    change uv to RG texture, fix output name

--- shaders/blitFrag.glsl
+++ shaders/blitFrag.glsl
@@ -8,14 +8,13 @@ out vec3 color;
 
 void main() {
 	vec2 coord = uv * .5 + vec2(.5);
-	color = texture(albedo, coord).xyz;
 	
-	/*
-	if (coord.x < 1. / 3.)
+	if (coord.x < 1. / 4.)
 		color = vec3(abs(-.005 / abs(texture(depth, coord).x-1.)));
-	else if (coord.x < 2. / 3.)
+	else if (coord.x < 2. / 4.)
 		color = texture(texcoord, coord).xyy; // color = texture(normal, coord).xyz*.5+vec3(.5);
+	else if (coord.x < 3. / 4.)
+		color = texture(texcoord, coord).xyz;
 	else
 		color = texture(albedo, coord).xyz;
-	*/
 }
--- shaders/objFrag.glsl
+++ shaders/objFrag.glsl
@@ -4,7 +4,7 @@
 in vec3 albedo, position, normal_varying;
 in vec2 uv_varying;
 out vec3 color, normal;
-out vec3 uv;
+out vec2 uv;
 
 uniform sampler2D logo;
 
@@ -12,9 +12,6 @@ void main() {
 	color = vec3(1.)-texture(logo, vec2(.0,1.)+vec2(1.,-1.)*clamp(position.xy*vec2(-2.,2.)-vec2(-1.0,.1), vec2(.0), vec2(1.))).xyz;
 	if (position.z < .55) color = vec3(1.);
 	color *= albedo;
-	color = vec3(uv.xy, 0.);
 	normal = normal_varying;
-	uv.xy = uv_varying;
-	
-	color = vec3(uv.xy, 1.0);
+	uv = uv_varying;
 }
